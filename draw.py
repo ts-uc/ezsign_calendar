0cd9232fc5efd2322bbba757c2e06a9a75acb58c
Nono Yuu <s0@ts-uc.org>
2026-05-16 10:01:41 +0900

    中央揃えになるよう修正

--- draw.py
+++ draw.py
@@ -10,17 +10,17 @@ class Draw:
     def draw_cell(self, x: int, y: int, w: int, h: int, text: str, text_size: int, red: bool = False):
         font = read_font.read_font(text_size)
 
-        bbox = self.draw.textbbox((0, 0), text, font=font)
-        text_w = bbox[2] - bbox[0]
-        text_h = bbox[3] - bbox[1]
-        tx = x + (w - text_w) // 2 - bbox[0]
-        ty = y + (h - text_h) // 2 - bbox[1]
+        # セルの中央座標
+        cx = x + w // 2
+        cy = y + h // 2
 
+        # anchor="mm" で見えているサイズの中央に配置
         self.draw.text(
-            (tx, ty),
+            (cx, cy),
             text,
             font=font,
-            fill=(255, 0, 0) if red else (0, 0, 0)
+            fill=(255, 0, 0) if red else (0, 0, 0),
+            anchor="mm"
         )
 
     def save(self, out: str):
